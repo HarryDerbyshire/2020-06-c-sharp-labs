aa081fd58866cfe4f765fe1da4d837293b2ed90c
HarryDerbyshire <harry@derbyshire.uk>
2020-08-06 11:06:01 +0100

    Added overnight API work

---
+++ tasks/PostCodeApiController/PostCodeApiController/Program.cs
@@ -0,0 +1,99 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+
+namespace PostCodeApiController
+{
+    public class Program
+    {
+        static List<Root> locations = new List<Root>();
+        //public static Uri url = new Uri($"https://api.postcodes.io/postcodes/{postcode}");
+       
+        static void Main(string[] args)
+        {
+            //GetLocation();
+            
+           
+        }
+        
+        public static bool ValidatePostcode(Uri url)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var data = httpClient.GetStringAsync(url);
+                Validate myDeserializedClass = JsonConvert.DeserializeObject<Validate>(data.Result);
+                return myDeserializedClass.result;
+                //Console.WriteLine(myDeserializedClass.result.country); 
+            }
+        }
+
+        public static Root GetLocation(string postcode)
+        {
+            Uri url = new Uri($"https://api.postcodes.io/postcodes/{postcode}");
+            
+            
+            using (var httpClient = new HttpClient())
+            {            
+                var data = httpClient.GetStringAsync(url);
+                Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(data.Result);
+                return myDeserializedClass;
+                //Console.WriteLine(myDeserializedClass.result.country); 
+            }
+        }
+    }
+
+    public class Codes
+    {
+        public string admin_district { get; set; }
+        public string admin_county { get; set; }
+        public string admin_ward { get; set; }
+        public string parish { get; set; }
+        public string parliamentary_constituency { get; set; }
+        public string ccg { get; set; }
+        public string ccg_id { get; set; }
+        public string ced { get; set; }
+        public string nuts { get; set; }
+    }
+
+    public class Result
+    {
+        public string postcode { get; set; }
+        public int quality { get; set; }
+        public int eastings { get; set; }
+        public int northings { get; set; }
+        public string country { get; set; }
+        public string nhs_ha { get; set; }
+        public double longitude { get; set; }
+        public double latitude { get; set; }
+        public string european_electoral_region { get; set; }
+        public string primary_care_trust { get; set; }
+        public string region { get; set; }
+        public string lsoa { get; set; }
+        public string msoa { get; set; }
+        public string incode { get; set; }
+        public string outcode { get; set; }
+        public string parliamentary_constituency { get; set; }
+        public string admin_district { get; set; }
+        public string parish { get; set; }
+        public string admin_county { get; set; }
+        public string admin_ward { get; set; }
+        public string ced { get; set; }
+        public string ccg { get; set; }
+        public string nuts { get; set; }
+        public Codes codes { get; set; }
+    }
+
+    public class Root
+    {
+        public int status { get; set; }
+        public Result result { get; set; }
+    }
+
+    public class Validate
+    {
+        public int status { get; set; }
+        public bool result { get; set; }
+    }
+}
---
+++ tasks/PostCodeApiController/PostCodeApiView/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PostCodeApiController;
+
+namespace PostCodeApiView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static Root currentAddress = new Root();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonFind_Click(object sender, RoutedEventArgs e)
+        {
+            Uri validateUrl = new Uri($"https://api.postcodes.io/postcodes/{TextBoxPostcode.Text}/validate");
+            bool isValid = Program.ValidatePostcode(validateUrl);
+            if (isValid == true)
+            {
+                if (TextBoxPostcode.Text != "" && TextBoxPostcode.Text != "Not a valid postcode")
+                {
+                    ButtonOpen.IsEnabled = true;
+                    ButtonOpen.Visibility = Visibility.Visible;
+                    currentAddress = Program.GetLocation(TextBoxPostcode.Text);
+                    LabelConstituency.Content = currentAddress.result.parliamentary_constituency;
+                    LabelCountry.Content = currentAddress.result.country;
+                    LabelNhsRegion.Content = currentAddress.result.nhs_ha;
+                    LabelDistrict.Content = currentAddress.result.admin_district;
+                    LabelWard.Content = currentAddress.result.admin_ward;
+                }
+
+            }
+            else
+            {
+                ButtonOpen.IsEnabled = false;
+                ButtonOpen.Visibility = Visibility.Hidden;
+                TextBoxPostcode.Text = "Not a valid postcode";
+                LabelConstituency.Content = "";
+                LabelCountry.Content = "";
+                LabelNhsRegion.Content = "";
+                LabelDistrict.Content = "";
+                LabelWard.Content = "";
+            }
+
+
+            }
+
+        private void ButtonOpen_Click(object sender, RoutedEventArgs e)
+        {            
+            Process.Start("explorer", $"https://www.google.com/maps/place/{TextBoxPostcode.Text}");
+        }
+    }
+}
