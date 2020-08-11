7de443d6088bcc225fc91f49ac9780080d0005fe
HarryDerbyshire <harry@derbyshire.uk>
2020-08-11 09:10:14 +0100

    Added VideoGameAPI project

---
+++ tasks/VideoGameAPI/VideoGameAPI/Models/Platform.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VideoGameAPI.Models
+{
+    class Platform
+    {
+        public Platform()
+        {
+            Games = new HashSet<Game>();
+        }
+        public int PlatformId { get; set; }
+        public string PlatformName { get; set; }
+
+        public ICollection<Game> Games { get; set; }
+    }
+}
